28c6593181e84cc38666b840ac330f9d77a44c06
Nick Graffis <nicholasgraffis@gmail.com>
2021-05-12 14:19:25 -0700

    🐚 Shell Started. Need to test...

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{ts,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme:  {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
